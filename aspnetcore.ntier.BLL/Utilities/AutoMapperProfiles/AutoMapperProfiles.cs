3c42a28faa7c9a6ffb756fbde724af8e5f77b09d
aghayeffemin <aghayeff.emin@gmail.com>
2023-01-15 14:26:41 +0100

    added automapper and customexceptions

---
+++ aspnetcore.ntier.BLL/Utilities/AutoMapperProfiles/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+﻿using aspnetcore.ntier.DTO.DTOs;
+using aspnetcore.ntier.Entity.Entities;
+using AutoMapper;
+
+namespace aspnetcore.ntier.BLL.Utilities.AutoMapperProfiles;
+
+public static class AutoMapperProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDTO>().ReverseMap();
+            CreateMap<User, UserToAddDTO>().ReverseMap();
+        }
+    }
+}
---
+++ aspnetcore.ntier.BLL/Utilities/CustomExceptions/UserNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿namespace aspnetcore.ntier.BLL.Utilities.CustomExceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException()
+    {
+    }
+
+    public UserNotFoundException(string message)
+        : base(message)
+    {
+    }
+
+    public UserNotFoundException(string message, Exception inner)
+        : base(message, inner)
+    {
+    }
+}
