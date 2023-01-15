15979df8bee978aeb449f3d2a1295fbac9cf329c
aghayeffemin <aghayeff.emin@gmail.com>
2023-01-15 13:46:35 +0100

    created user entity model and dbcontext

---
+++ aspnetcore.ntier.DAL/DataContext/AspNetCoreNTierDbContext.cs
@@ -0,0 +1,24 @@
+﻿using aspnetcore.ntier.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnetcore.ntier.DAL.DataContext;
+
+public class AspNetCoreNTierDbContext : DbContext
+{
+    public AspNetCoreNTierDbContext(DbContextOptions<AspNetCoreNTierDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+             new User
+             {
+                 UserId = 1,
+                 Username = "John",
+                 Name = "Doe",
+                 Surname = "johndoe",
+             }
+         );
+    }
+}
---
+++ aspnetcore.ntier.Entity/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace aspnetcore.ntier.Entity.Entities;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string Username { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+}
